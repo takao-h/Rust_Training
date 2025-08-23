c536605738d6d2146dfd42c81436954051e72098
takao-h <takao.hayashi@zozo.com>
2025-08-23 10:04:52 +0900

    pass test

--- rust_training_course/src/lesson2_types_control_flow.rs
+++ rust_training_course/src/lesson2_types_control_flow.rs
@@ -1,3 +1,11 @@
+use std::i32;
+
 // ここに `check_number` 関数を実装してください。
 // i32型の引数を取り、その値に応じて "Positive", "Negative", "Zero" のいずれかのStringを返します。
-
+pub fn check_number(a: i32) -> String {
+    match a {
+        0 => "Zero".to_string(),
+        1..=i32::MAX => "Positive".to_string(),
+        i32::MIN..=-1 => "Negative".to_string(),
+    }
+}
